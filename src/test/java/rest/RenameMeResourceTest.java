be505c1a309e4c764411ef67320fb7ffb472ed2b
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-03 17:02:31 +0200

    Added @Disabled on Example Tests.

--- src/test/java/rest/RenameMeResourceTest.java
+++ src/test/java/rest/RenameMeResourceTest.java
@@ -23,7 +23,7 @@ import utils.EMF_Creator.DbSelector;
 import utils.EMF_Creator.Strategy;
 
 //Uncomment the line below, to temporarily disable this test
-//@Disabled
+@Disabled
 public class RenameMeResourceTest {
 
     private static final int SERVER_PORT = 7777;

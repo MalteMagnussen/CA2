7790a765811745aad9d77260654a1787b40783fb
HrBjarup <cph-ab363@cphbusiness.dk>
2019-10-04 15:26:36 +0200

    Added Hobby entity. Added empty constructor and relationship fields in Person entity

---
+++ src/main/java/entities/Hobby.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+/**
+ *
+ * @author
+ */
+@Entity
+public class Hobby implements Serializable
+{
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    private String name, description;
+    
+    @ManyToMany(mappedBy = "hobbys", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "person_id")
+    private List<Person> persons = new ArrayList();
+
+    public Hobby()
+    {}
+    
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public List<Person> getPersons()
+    {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons)
+    {
+        this.persons = persons;
+    }
+    
+    public void addPerson(Person person)
+    {
+        this.persons.add(person);
+    }
+    
+}

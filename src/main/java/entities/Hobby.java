f16afa34e2688c364e663000c9c64ad454a0d78a
HrBjarup <cph-ab363@cphbusiness.dk>
2019-10-04 15:27:52 +0200

    Changed GenerationType to IDENTITY in Hobby entity

--- src/main/java/entities/Hobby.java
+++ src/main/java/entities/Hobby.java
@@ -27,7 +27,7 @@ public class Hobby implements Serializable
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
     private String name, description;

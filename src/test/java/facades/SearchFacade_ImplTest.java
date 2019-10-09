e177f062f63333973a8c11ae2e261b277f71c1b6
Camilla <camilla.valerius@gmail.com>
2019-10-09 09:51:25 +0200

    test getPersonsInCity

--- src/test/java/facades/SearchFacade_ImplTest.java
+++ src/test/java/facades/SearchFacade_ImplTest.java
@@ -1,5 +1,6 @@
 package facades;
 
+import dto.CityInfoDTO_IN;
 import dto.PersonDTO_IN;
 import dto.PersonDTO_OUT;
 import entities.Address;
@@ -31,24 +32,23 @@ public class SearchFacade_ImplTest {
     private static ArrayList<Hobby> hobbies2 = new ArrayList();
     private static ArrayList<Hobby> hobbies3 = new ArrayList();
     private static ArrayList<Hobby> hobbies4 = new ArrayList();
-    
+
     private static ArrayList<Phone> phones1 = new ArrayList();
     private static ArrayList<Phone> phones2 = new ArrayList();
     private static ArrayList<Phone> phones3 = new ArrayList();
     private static ArrayList<Phone> phones4 = new ArrayList();
-    
+
     private static Person person1 = new Person("rigmor@email.dk", "Rigmor", "Noggenfogger", hobbies1);
     private static Person person2 = new Person("boris@email.dk", "Boris", "Ragnaros", hobbies2);
     private static Person person3 = new Person("zacharias@email.dk", "Zacharias", "Onyxia", hobbies3);
     private static Person person4 = new Person("flemse@email.dk", "Flemming", "Atramedes", hobbies4);
-    
+
     private static CityInfo city1 = new CityInfo("3400", "Hillerød");
     private static CityInfo city2 = new CityInfo("4000", "Roskilde");
     private static Address address1 = new Address("Kings Road 1", "By the Inn");
     private static Address address2 = new Address("Fishroad 7", "Next to the Lake");
     private static Address address3 = new Address("Fleabottom 69", "In the shitpit");
-    
-    
+
     public SearchFacade_ImplTest() {
     }
 
@@ -63,25 +63,25 @@ public class SearchFacade_ImplTest {
         hobbies2.add(new Hobby("WoW", "Det der spil"));
         hobbies3.add(new Hobby("Frimærker", "Samlerobjekt"));
         hobbies4.add(new Hobby("Sightseeing", "Samlerobjekt"));
-        
+
         phones1.add(new Phone(123456, "Home", person1));
         phones1.add(new Phone(987456, "Mobile", person1));
         phones2.add(new Phone(654789, "Home", person2));
         phones2.add(new Phone(365478, "Work", person2));
         phones3.add(new Phone(456789, "Mobile", person3));
         phones4.add(new Phone(333333, "Work", person4));
-        
+
         person1.addPhone(phones1.get(0));
         person1.addPhone(phones1.get(1));
         person2.addPhone(phones2.get(0));
         person2.addPhone(phones2.get(1));
         person3.addPhone(phones3.get(0));
         person4.addPhone(phones4.get(0));
-        
+
         address1.setCityinfo(city1);
         address2.setCityinfo(city2);
         address3.setCityinfo(city2);
-        
+
         person1.setAddress(address1);
         person2.setAddress(address2);
         person3.setAddress(address3);
@@ -192,47 +192,51 @@ public class SearchFacade_ImplTest {
         List<PersonDTO_OUT> result = facade.getPersonByFullName("Rigmor Noggenfogger");
         assertEquals(expResult, result.get(0));
     }
-    
+
     @Test
     public void testGetZipcodes() {
-     List<String> exp = new ArrayList();
-     exp.add("3400");
-     exp.add("4000");
-     assertEquals(exp, facade.getZipcodes());
+        List<String> exp = new ArrayList();
+        exp.add("3400");
+        exp.add("4000");
+        assertEquals(exp, facade.getZipcodes());
     }
-    
+
     @Test
     public void testGetPersonsInCity() {
-        
+        List<PersonDTO_OUT> exp = new ArrayList();
+        exp.add(new PersonDTO_OUT(new Person("rigmor@email.dk", "Rigmor", "Noggenfogger", hobbies1)));
+        exp.add(new PersonDTO_OUT(new Person("flemse@email.dk", "Flemming", "Atramedes", hobbies4)));
+        List<PersonDTO_OUT> result = facade.getPersonsInCity(new CityInfoDTO_IN(city1));
+        assertEquals(exp, result);
     }
-	
-	@Test
+
+    @Test
     public void testGetCities() {
-        
+
     }
-	
-	@Test
+
+    @Test
     public void testGetCityByName() {
-        
+
     }
-	
-	@Test
+
+    @Test
     public void testGetCityByZipCode() {
-        
+
     }
-	
-	@Test
+
+    @Test
     public void testCreateCity() {
-        
+
     }
-	
-	@Test
+
+    @Test
     public void testEditCity() {
-        
+
     }
-	
-	@Test
+
+    @Test
     public void testDeleteCity() {
-        
+
     }
 }

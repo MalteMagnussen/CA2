bb45d040dcc725af9e7288d70cbdfd17a8c50f53
Runi-VN <runi-vn@github.com>
2019-10-10 16:27:22 +0200

    getByPhone-add namedQuery

--- src/main/java/entities/Phone.java
+++ src/main/java/entities/Phone.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;

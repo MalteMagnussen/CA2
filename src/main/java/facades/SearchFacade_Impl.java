d4685bbd3b581f01dc73b3a90423c52b90c499ff
Camilla <camilla.valerius@gmail.com>
2019-10-10 19:31:02 +0200

    again.....

--- src/main/java/facades/SearchFacade_Impl.java
+++ src/main/java/facades/SearchFacade_Impl.java
@@ -168,7 +168,6 @@ public class SearchFacade_Impl implements ISearchFacade {
                         city.setCity(personDTO.getAddress().getCityInfo().getCity());
                         city.setZipCode(personDTO.getAddress().getCityInfo().getZipCode());
                         city = em.merge(city);
-                        address.setCityinfo(city);
                     }
                     address.setCityinfo(city);
                 }

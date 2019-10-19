59d195bdf0f299d1e22a48a7cd9cf3923cdcee40
HrBjarup <cph-ab363@cphbusiness.dk>
2019-10-19 19:53:01 +0200

    Fixed addAll method adding duplicates when name of existing element is entered but with extra white space

--- src/main/java/facades/SearchFacade_Impl.java
+++ src/main/java/facades/SearchFacade_Impl.java
@@ -297,7 +297,7 @@ public class SearchFacade_Impl implements ISearchFacade {
         }
 
         // Create Person
-        Person person = new Person(personDTO.getEmail(), personDTO.getFirstName(), personDTO.getLastName());
+        Person person = new Person(personDTO.getEmail().trim(), personDTO.getFirstName().trim(), personDTO.getLastName().trim());
 
         // Add Phones to Person. 
         // They are unique for each person, so no need to check DB for these. 
@@ -316,8 +316,8 @@ public class SearchFacade_Impl implements ISearchFacade {
             // Check if Hobby already exists to avoid duplicates.
             // If it does, we manage it with JPA. 
             personDTO.getHobbies().forEach((hobbyDTO) -> {
-                String description = hobbyDTO.getDescription();
-                String name = hobbyDTO.getName();
+                String description = hobbyDTO.getDescription().trim();
+                String name = hobbyDTO.getName().trim();
                 Hobby hobby = getHobby(name, description);
                 // If Hobby doesn't exist in database.
                 if (hobby == null) {
@@ -328,16 +328,16 @@ public class SearchFacade_Impl implements ISearchFacade {
             });
 
             // Check if address already exists. 
-            String street = personDTO.getAddress().getStreet();
-            String additionalInfo = personDTO.getAddress().getAdditionalInfo();
+            String street = personDTO.getAddress().getStreet().trim();
+            String additionalInfo = personDTO.getAddress().getAdditionalInfo().trim();
             Address address = getAddress(street, additionalInfo);
             if (address == null) {
                 address = new Address(street, additionalInfo);
             }
 
             // Check if City already exists. 
-            String zipCode = personDTO.getAddress().getCityInfo().getZipCode();
-            String cityName = personDTO.getAddress().getCityInfo().getCity();
+            String zipCode = personDTO.getAddress().getCityInfo().getZipCode().trim();
+            String cityName = personDTO.getAddress().getCityInfo().getCity().trim();
             CityInfo cityInfo = getCity(cityName, zipCode);
             if (cityInfo == null) {
                 cityInfo = new CityInfo(zipCode, cityName);
